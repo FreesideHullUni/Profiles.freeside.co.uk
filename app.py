e533552a5b964ce350e18c13f5118adc209523fc
Kieran Coldron <kieran@coldron.com>
2018-03-25 04:39:31 +0100

    Change display name and forename min length

--- app.py
+++ app.py
@@ -27,8 +27,8 @@ class EmailForm(Form):
     email = StringField('Email Address', [validators.Length(min=6, max=35)])
 
 class RegisterForm(Form):
-    display_name = StringField('Display Name', validators=[validators.Length(min=4, max=35),validators.DataRequired()])
-    first_name = StringField('Forename', validators=[validators.Length(min=4, max=35),validators.DataRequired()])
+    display_name = StringField('Display Name', validators=[validators.Length(min=2, max=35),validators.DataRequired()])
+    first_name = StringField('Forename', validators=[validators.Length(min=2, max=35),validators.DataRequired()])
     password = PasswordField('Password', [
         validators.Length(min=8),
         validators.DataRequired(),
